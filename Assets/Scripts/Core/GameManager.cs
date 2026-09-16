a5b3770768f206695ad200651df3ea06b741f9fa
tailorsky <152508194+tailorsky@users.noreply.github.com>
2026-09-16 20:16:06 +0300

    add game over and restart

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public bool IsGameOver { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            Restart();
+    }
+
+    public void GameOver()
+    {
+        if (IsGameOver)
+            return;
+
+        IsGameOver = true;
+        Time.timeScale = 0f;
+
+        Debug.Log("GAME OVER");
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/Core/LoseZone.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class LoseZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Enemy>() != null)
+            GameManager.Instance.GameOver();
+    }
+}
