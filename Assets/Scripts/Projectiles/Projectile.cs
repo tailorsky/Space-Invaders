1dc6ee90deeb9495f2e8ebb55409b85d231dba70
tailorsky <152508194+tailorsky@users.noreply.github.com>
2026-09-16 20:11:40 +0300

    add enemy damage

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -4,6 +4,7 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private float speed = 8f;
     [SerializeField] private float lifeTime = 3f;
+    [SerializeField] private int damage = 1;
 
     private void Start()
     {
@@ -13,5 +14,16 @@ public class Projectile : MonoBehaviour
     private void Update()
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Enemy enemy = other.GetComponent<Enemy>();
+
+        if (enemy == null)
+            return;
+
+        enemy.TakeDamage(damage);
+        Destroy(gameObject);
     }
 }
